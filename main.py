3c2ec6223532d0364dd1af82476773e9ed929c92
David Hochstetler <daviddevs23@gmail.com>
2021-02-24 18:19:36 -0500

    Redid everything

--- main.py
+++ main.py
@@ -1,40 +1,20 @@
 import discord
+from discord.ext import commands
 
-# A function to grab items from token.txt
+# Import Commands
+from extensions.startup import Startup
+
+# Command for parcing token.txt
 def get_token(index):
     with open("token.txt", "r") as f:
         lines = f.readlines()
         return lines[index].strip()
 
-client = discord.Client()
+# Set the command prefix dynamically when initializing bot
+client = commands.Bot(command_prefix=get_token(0))
 
-TOKEN = get_token(1)
-DELIMIN = get_token(0)
+# Add the extensions to the main bot
+client.add_cog(Startup(client))
 
-@client.event
-async def on_ready():
-    print(f'Bot {client.user} is now online')
+client.run(get_token(1))
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-    if message.content.startswith(f'{DELIMIN}hello'):
-        name = str(message.author)
-        name = name.split('#')[0]
-        await message.channel.send(f'Hello {name}')
-
-    elif message.content.startswith(f'{DELIMIN}test'):
-        await message.channel.send('TEST TEST TEST')
-    
-    elif message.content.startswith(f'{DELIMIN}test2'):
-        await message.channel.send('TEST2 TEST2 TEST2')
-
-    ## Add commands here
-
-@client.event
-async def on_member_join(member):
-    await member.send('Private message')
-
-client.run(TOKEN)
