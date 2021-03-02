c14e7f4f224b023cadbd7e04528ea5b93e557e07
patrick15965 <63556707+patrick15965@users.noreply.github.com>
2021-03-02 13:59:13 -0500

    Updated Dad
    I had a merge conflict so I'm just overwriting the old one in here

--- extensions/dad.py
+++ extensions/dad.py
@@ -1,22 +1,51 @@
 import discord
+import os
 import random
 from discord.ext import commands
 
+#   dad.py is a cog class which holds two functions, one to check messages for "I'm"
+#   and another that sends a dad joke when .dad command is called
+
 class Dad(commands.Cog):
 
+    # Initializer function
     def __init__(self, client):
         self.client = client
 
+    # Listener to confirm bot has booted properly
     @commands.Cog.listener()
     async def on_ready(self):
-        print('Bot is online.')
+        print('Dad Bot is online.')
 
-    @commands.command()
-    async def ping(self, ctx):
-        await ctx.send('Pong!')
+    # Listener that checks messages for "I'm"
+    @commands.Cog.listener()
+    async def on_message(self, message):
 
+        # Convert message object to string
+        content = message.content
+
+        # Check if message was sent by dad.py, ignore if it was
+        if 'Dad Bot' in content:
+            return
+
+        # Check if message contains keyword "I'm"
+        if 'I\'m' in content:
+
+            # Split string after "I'm"
+            aftIm = content.split("I'm ", 1)[1]
+
+            # Formatted response
+            response = f'Hi {aftIm.capitalize()}, I\'m Dad Bot!'
+
+            # send message to channel
+            await message.channel.send(response)
+            return
+
+    # Command for dad joke
     @commands.command()
     async def dad(self, ctx):
+
+        # List of dad jokes
         dadJokes = ['What do you call a Mexican who lost his car? Carlos.',
                     'What do you call a fake noodle? An Impasta.',
                     'People say they pick their nose, but I feel like I was born with mine.',
@@ -36,7 +65,18 @@ class Dad(commands.Cog):
                     'Why do crabs never give to charity? Because they’re shellfish.',
                     'What do you call a fish with no eyes? A fshhhh.',
                     'What do you call a man with no arms and no legs lying in front of your door? Matt.',
-                    
+                    'I told my girlfriend she drew her eyebrows too high. She seemed surprised.',
+                    'I bought my friend an elephant for his room. He said \"Thanks\". I said \"Don\'t mention it\".',
+                    'I poured root beer in a square glass. Now I just have beer',
+                    'I have an EpiPen. My friend gave it to me when he was dying. It seemed very important to him that I have it.',
+                    'So what if I don\'t want to know what Armageddon means? It\'s not the end of the world.',
+                    'This is my step ladder. I never knew my real ladder.',
+                    'I bought the world\'s worst thesaurus yesterday. Not only is it terrible, it\'s terrible.',
+                    'I have the heart of a lion and a lifetime ban from the Toronto zoo.'
                     ]
+        # Return random dad joke from list
         await ctx.send(f'{random.choice(dadJokes)}')
 
+# Class setup function
+def setup(client):
+    client.add_cog(Dad(client))
