ae02bf988a306bc492ef95474301761d273256e9
patrick15965 <63556707+patrick15965@users.noreply.github.com>
2021-03-22 12:21:36 -0400

    Story function

---
+++ extensions/story.py
@@ -0,0 +1,69 @@
+import discord
+import os
+import praw
+import random
+import requests
+import re
+from discord.ext import commands
+
+
+# Grab access info for Reddit API
+def tokens(self, index):
+    with open("token.txt", "r") as f:
+        lines = f.readlines()
+        return lines[index].strip()
+
+
+class Story(commands.Cog):
+
+    # intialize client
+    def __init__(self, client):
+        self.client = client
+
+
+    # story command
+    @commands.command()
+    async def story(self, ctx):
+
+        # initialize praw
+        reddit = praw.Reddit(
+                            client_id=tokens(self, 4),
+                            client_secret=tokens(self, 5),
+                            user_agent="TeamHBot"
+                            )
+
+        # List of subreddits
+        subs =  [
+                    "Microfiction",
+                    "TwoSentenceHorror",
+                    "TwoSentenceComedy"
+                ]
+
+        # Choose random subreddit from subs
+        sub = reddit.subreddit(random.choice(subs))
+        posts = sub.hot(limit = 50)
+
+        # Empty list to store posts
+        stories = []
+
+        # url and filname initialized for embedding
+        title = ""
+        contents = ""
+
+        # Get rid of pinned posts and append to stories
+        for post in posts:
+            if not post.stickied and not post.over_18:
+                stories.append(post)
+
+        rand_story = random.choice(stories)
+
+        title = rand_story.title
+        contents = rand_story.selftext
+
+        print(title)
+
+        em = discord.Embed(title = title, description = contents)
+
+        await ctx.send(embed = em)
+
+
